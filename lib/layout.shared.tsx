84e5d2676a9755131c056d4cbb2125f9c4149a45
Clickism <yagizaktasx@gmail.com>
2026-06-11 17:05:22 +0200

    Initial commit

---
+++ lib/layout.shared.tsx
@@ -0,0 +1,34 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { appName, gitConfig } from './shared';
+import { customIcons } from './icons';
+
+export function baseOptions(): BaseLayoutProps {
+  return {
+    nav: {
+      title: (
+        <>
+          {customIcons.clickism}
+          {appName}
+        </>
+      ),
+    },
+    links: [
+      {
+        text: 'Documentation',
+        url: '/docs',
+        on: 'nav',
+      },
+      {
+        type: 'icon',
+        icon: customIcons.discord,
+        external: true,
+        url: 'https://discord.gg/zUetzp3Gzk',
+        text: 'Discord',
+        label: 'Discord',
+        on: 'all',
+      }
+    ],
+    githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+
+  };
+}
