99dbbfd4437a5f782038b790a51b3aaf20b1f3aa
Clickism <yagizaktasx@gmail.com>
2026-06-11 17:21:25 +0200

    add wip warning to homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,18 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
+      <h1 className="text-2xl font-bold mb-4">Clickism</h1>
       <p>
-        Hello there!
+        The home page is work in progress!
+      </p>
+      <p>
+        But in the mean time you can check out the
+        {" "}
+        <Link href="/docs" className="text-fd-primary underline">
+          documentation
+        </Link>
+        {" "}
+        for my projects!
       </p>
     </div>
   );
