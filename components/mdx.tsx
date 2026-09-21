1e4bdcebed0cdd6a787123fc2c6a5845c0603abc
Clickism <yagizaktasx@gmail.com>
2026-09-22 00:24:03 +0200

    add showcase

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,14 +1,22 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
-import type {MDXComponents} from "mdx/types";
-import {Callout} from "fumadocs-ui/components/callout";
+import type { MDXComponents } from "mdx/types";
+import { Callout } from "fumadocs-ui/components/callout";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
     ...components,
-    blockquote: (props) => (
-      <Callout>{props.children}</Callout>
-    )
+    img: (props) => (
+      <ImageZoom
+        {...(props as any)}
+        className="rounded-2xl border border-white/20"
+        rmiz={{
+          canSwipeToUnzoom: true,
+        }}
+      />
+    ),
+    blockquote: (props) => <Callout>{props.children}</Callout>,
   } satisfies MDXComponents;
 }
 
