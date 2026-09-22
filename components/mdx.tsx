1e184b0071c9527cca5c12eef9b83e2fd615aeaf
Clickism <yagizaktasx@gmail.com>
2026-09-22 02:52:51 +0200

    add pack intro and static textures

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -2,6 +2,13 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { Callout } from "fumadocs-ui/components/callout";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
+import {
+  File as FumaFile,
+  FileProps,
+  Files as FumaFiles,
+  Folder as FumaFolder,
+} from "fumadocs-ui/components/files";
+import { Braces } from "lucide-react";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
@@ -25,3 +32,18 @@ export const useMDXComponents = getMDXComponents;
 declare global {
   type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
 }
+
+export function File(props: FileProps) {
+  const icons: Record<string, React.ReactNode> = {
+    json: <Braces />,
+    mcmeta: <Braces />,
+  };
+  const extension = props.name.split(".").pop()?.toLowerCase();
+  if (extension && extension in icons) {
+    return <FumaFile {...props} {...{ icon: icons[extension] }} />;
+  }
+  return <FumaFile {...props} {...{}} />;
+}
+
+export const Folder = FumaFolder;
+export const Files = FumaFiles;
